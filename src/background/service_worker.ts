b24bec5a72ccc6b2d5c78676b2608b09471eee7f
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2025-02-23 22:16:49 +0900

    first commit

---
+++ src/background/service_worker.ts
@@ -0,0 +1,17 @@
+chrome.sidePanel
+  .setPanelBehavior({ openPanelOnActionClick: true })
+  .catch((error) => console.error(error));
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "openSidePanel",
+    title: "Open Side Panel",
+    contexts: ["all"],
+  });
+
+  chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "openSidePanel") {
+      chrome.sidePanel.open({ windowId: tab!.windowId });
+    }
+  });
+});
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: "src",
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        sidePanel: "/sidePanel/index.html",
+        background: "/background/service_worker.ts",
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash][extname]",
+      },
+    },
+  },
+});
